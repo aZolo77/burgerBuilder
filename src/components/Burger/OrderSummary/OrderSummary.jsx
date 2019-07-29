73b0a8be50c0a9a06af6112b2ac24e759d446bbc
Zolo <a.burtsev@hostland.ru>
2019-07-29 12:40:33 +0300

    Improvements with folder structure and hooks

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,41 +1,50 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = ({
-  ingredients,
-  purchaseCanceled,
-  purchaseContinued,
-  price = 0
-}) => {
-  const ingredientSummary = Object.keys(ingredients).map(igKey => {
-    if (ingredients[igKey] !== 0) {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-          {ingredients[igKey]}
-        </li>
-      );
-    }
-    return null;
-  });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious Burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={purchaseCanceled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+class OrderSummary extends Component {
+  // componentWillUpdate() {
+  //   console.log('OrderSummary was updated');
+  // }
+
+  render() {
+    const {
+      ingredients,
+      purchaseCanceled,
+      purchaseContinued,
+      price
+    } = this.props;
+
+    const ingredientSummary = Object.keys(ingredients).map(igKey => {
+      if (ingredients[igKey] !== 0) {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+            {ingredients[igKey]}
+          </li>
+        );
+      }
+      return null;
+    });
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious Burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={purchaseCanceled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
 
 export default OrderSummary;
--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.css';
 
-const Modal = ({ children, show, modalClosed }) => (
-  <Aux>
-    <Backdrop show={show} clicked={modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: show ? '1' : '0'
-      }}
-    >
-      {children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, _) {
+    return nextProps.show !== this.props.show;
+  }
+
+  // componentWillUpdate() {
+  //   console.log('modal was updated');
+  // }
+
+  render() {
+    const { children, show, modalClosed } = this.props;
+    return (
+      <Aux>
+        <Backdrop show={show} clicked={modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: show ? '1' : '0'
+          }}
+        >
+          {children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 export default Modal;
